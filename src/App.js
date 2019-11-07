3ffd7c87921c9551952cbb3b967d68326e6b30f5
Amit Dubey <amit.dubey@globant.com>
2019-11-07 18:33:13 +0530

    Adds boilerplate for react assignment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>All set for Code Realm</h2>
+    </div>
+  );
+}
+
+export default App;
